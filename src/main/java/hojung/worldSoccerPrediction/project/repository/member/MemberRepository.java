9d9339e80adb1baa3683e5cc2e21499175d1e3aa
lcj1204 <dlckdwo1204@naver.com>
2022-09-14 00:23:53 +0900

    FEAT: initial

--- src/main/java/hojung/worldSoccerPrediction/project/repository/member/MemberRepository.java
+++ src/main/java/hojung/worldSoccerPrediction/project/repository/member/MemberRepository.java
@@ -1,4 +1,14 @@
 package hojung.worldSoccerPrediction.project.repository.member;
 
+import hojung.worldSoccerPrediction.project.domain.member.Member;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByNickname(String alias);
+    Optional<Member> findByEmail(String email);
+    List<Member> findAll();
 }
