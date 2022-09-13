281a6f201bffd782471865d39758faab88734818
lcj1204 <dlckdwo1204@naver.com>
2022-09-14 00:22:45 +0900

    REFACTOR: addPrediction 파라메터 순서 변경

--- src/main/java/hojung/worldSoccerPrediction/project/service/prediction/PredictionService.java
+++ src/main/java/hojung/worldSoccerPrediction/project/service/prediction/PredictionService.java
@@ -5,7 +5,7 @@ import hojung.worldSoccerPrediction.project.domain.prediction.Prediction;
 import java.util.List;
 
 public interface PredictionService {
-    Prediction addPrediction(Long memberId, Prediction prediction);// 예측 생성
+    Prediction addPrediction(Prediction prediction, Long memberId);// 예측 생성
 
     List<Prediction> getPredictionByMemberId(Long memberId);// 회원으로 예측 조회 -> Member
 
